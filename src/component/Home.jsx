047a182c5847e1582c6d57e7f5ef3a16580a1d88
Jahidulislam23 <jahidul110j@gmail.com>
2025-08-10 18:10:35 +0600

    added the home page featuresSection

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -15,9 +15,6 @@ const Home = () => {
     
     return (
         <div>
-            <div className='px-7 py-5  sm:pr-3 lg:mx-auto lg:items-center md:mx-auto md:items-center sm:mx-auto sm:items-center'>
-                <FeaturesSection></FeaturesSection>
-            </div>
             <div className='py-5 px-7'>
                 <HomeSlider></HomeSlider>
             </div>
@@ -47,65 +44,12 @@ const Home = () => {
             <div className='py-5 px-7'>
                 <OnlineGroupStudy></OnlineGroupStudy>
             </div>
+            <div className='px-7 py-5  sm:pr-3 lg:mx-auto lg:items-center md:mx-auto md:items-center sm:mx-auto sm:items-center'>
+                <FeaturesSection></FeaturesSection>
+            </div>
            
         </div>
     );
 };
 
 export default Home;
-// // import FeaturesSection from "./FeaturesSection";
-// import React from 'react';
-// import HomeSlider from './HomeSlider';
-// import Banner from './Banner';
-// import FaqSection from './FaqSection';
-// import FeaturesSection from './Featuresection'
-// import {motion} from 'motion/react'
-// import { useLoaderData } from 'react-router';
-// import BannerSection from './BannerSection';
-
-// export default function Home({ data }) {
-//   return (
-//     <div className="space-y-20">
-      
-//       {/* Features */}
-      
-//       <FeaturesSection />
-
-//       {/* Slider */}
-//       <HomeSlider />
-
-//       {/* Banner Section */}
-//       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6">
-//         {data.map(card => (
-//           <BannerSection key={card._id} card={card} />
-//         ))}
-//       </div>
-
-//       {/* FAQ */}
-//       <div>
-//         <motion.h1 className="text-center text-6xl py-3">
-//           <span className="text-red-500">F</span>{" "}
-//           <span className="text-blue-500">A</span>{" "}
-//           <span className="text-green-500">Q</span>{" "}
-//           <motion.span
-//             animate={{
-//               color: ["#ff5733", "#33ff33", "#Ba33ff"],
-//               transition: { duration: 4, repeat: Infinity },
-//             }}
-//           >
-//             section
-//           </motion.span>
-//         </motion.h1>
-//         <FaqSection />
-//       </div>
-
-//       {/* Core Features */}
-//       <div>
-//         <h1 className="text-center text-6xl py-3">
-//           Educoda <span className="text-blue-500">Core</span> Features
-//         </h1>
-//         <Banner />
-//       </div>
-//     </div>
-//   );
-// }
